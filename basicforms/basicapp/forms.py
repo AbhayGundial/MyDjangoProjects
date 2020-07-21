d74afbc4f2e757abb933e4c8af5c5316c74fdba7
AbhayGundial <gundialabhay@gmail.com>
2020-07-21 23:23:05 +0530

    Added forms in basicapp

--- basicforms/basicapp/forms.py
+++ basicforms/basicapp/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField(max_length=200, required=False)
     email = forms.EmailField(required=False)
+    verify_email = forms.EmailField(label='Enter your email again:', required=False)
     text = forms.CharField(widget=forms.Textarea , required=False)
-    
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['verify_email']
+
+        if email != vemail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")
